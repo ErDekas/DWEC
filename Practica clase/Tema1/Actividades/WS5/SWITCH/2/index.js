98b456219d6fff7130bd9646906f348a4e5b9027
ErDekas <pabletor0505@gmail.com>
2024-09-19 08:37:41 +0200

    Arreglos de los ejercicios

--- Practica clase/Tema1/Actividades/WS5/SWITCH/2/index.js
+++ Practica clase/Tema1/Actividades/WS5/SWITCH/2/index.js
@@ -1,20 +1,44 @@
-let numero = parseInt(prompt("Introduce un numero"));
-switch(numero){
-    case par:
-        if(numero%2==0){
-            document.write("Es par");
-        }
+let numero = parseInt(prompt("Introduce un número:"));
+
+let codigo = 0;
+
+if (numero % 2 === 0) {
+    codigo += 1;
+}
+
+if (numero % 3 === 0) {
+    codigo += 2;
+}
+
+if (numero % 5 === 0) {
+    codigo += 4;
+}
+
+switch (codigo) {
+    case 0:
+        document.write(numero + ` no es par, ni múltiplo de 3, ni múltiplo de 5.`);
         break;
-    case multiplo3:
-        if(numero/3==0){
-            document.write("Es multiplo de 3");
-        }
+    case 1:
+        document.write(numero + ` es par, pero no es múltiplo de 3 ni de 5.`);
         break;
-    case multiplo5:
-        if(numero/5==0){
-            document.write("Es multiplo de 5");
-        }
+    case 2:
+        document.write(numero + ` es múltiplo de 3, pero no es par ni múltiplo de 5.`);
+        break;
+    case 3:
+        document.write(numero + ` es par y múltiplo de 3, pero no de 5.`);
+        break;
+    case 4:
+        document.write(numero + ` es múltiplo de 5, pero no es par ni múltiplo de 3.`);
+        break;
+    case 5:
+        document.write(numero + ` es par y múltiplo de 5, pero no de 3.`);
+        break;
+    case 6:
+        document.write(numero + ` es múltiplo de 3 y de 5, pero no es par.`);
+        break;
+    case 7:
+        document.write(numero + ` es par, múltiplo de 3 y múltiplo de 5.`);
         break;
     default:
-        document.write("No es multiplo de 3 ni de 5 y es impar");
+        document.write("Algo salió mal.");
 }

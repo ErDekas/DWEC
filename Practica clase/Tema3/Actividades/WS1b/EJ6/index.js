c1080915c7f2045abf28d6903e0fddb58a2c834c
Deka <150124896+ErDekas@users.noreply.github.com>
2024-10-08 09:36:28 +0200

    Arreglo ejercicio 6

--- Practica clase/Tema3/Actividades/WS1b/EJ6/index.js
+++ Practica clase/Tema3/Actividades/WS1b/EJ6/index.js
@@ -1,5 +1,1 @@
-const returnStringAge = age =>{
-    let edad = prompt(parseInt("Introduce tu edad"));
-    let frase = console.log(`Hello, I am name, and I am "${edad}" years old.`)
-    return frase;
-}
+const introduce = (name, age) => `Hello, I am ${name}, and I am ${age} years old.`;
